38732e5d796eebd0486724f7e5007574ec38ab3b
Selamawitl lemma <oneiconforlife26@gmail.com>
2020-08-21 22:07:51 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,5 +1,5 @@
 rescue_princess_peach
-def rescue_princess_peach(NameError)
+def rescue_princess_peach
   NameError = status
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
